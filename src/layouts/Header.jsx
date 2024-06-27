f8e1970c9109b7618dd36f3d3847c7682fe74713
Rasyid1609 <rasyid.mahardian@gmail.com>
2024-06-27 16:37:30 +0700

    update

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -44,7 +44,7 @@ export default function Header() {
                       download={true}
                       className='font-poppins font-semibold text-zinc-700 dark:text-slate-300 flex items-center gap-2 rounded-lg px-4 py-2 bg-slate-100 hover:bg-slate-200 dark:hover:bg-zinc-950 dark:bg-zinc-900 dark:border-zinc-700 border'
                     >
-                      LIHAT CV
+                      DOWNLOAD CV
                     </a>
                   </button>
                 </div>
